aee97f990ccdefe7c7e618a441babb04a7201d5c
Lukasz Cettler <lukasz.cettler@gmail.com>
2022-07-23 15:06:04 +0200

    Initial Commit

---
+++ TaskManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskManager
+//
+//  Created by Łukasz Cettler on 23/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
